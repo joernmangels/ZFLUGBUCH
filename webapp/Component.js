eeb4d4b86ec051d5242280d182f4795db23f25d5
Jörn Mangels <joern.mangels@varelmann.de>
2020-03-30 15:42:21 +0000

    Refresh 1

--- webapp/Component.js
+++ webapp/Component.js
@@ -3,7 +3,7 @@ sap.ui.define([
 	"sap/ui/Device",
 	"de/mangels/zfb/ZFLUGBUCH/model/models",
 	"sap/ui/model/json/JSONModel"
-	
+
 ], function (UIComponent, Device, models, JSONModel) {
 	"use strict";
 
@@ -21,27 +21,27 @@ sap.ui.define([
 		init: function () {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
-			
+
 			// enable routing
 			this.getRouter().initialize();
-			
+
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
-			
+
 			//SetUp HostName
 			var targetHostname, targetprotocol;
 			var currentHostname = window.location.hostname;
 			//if (currentHostname === "localhost" || currentHostname === "192.168.2.200") {
 			//if (currentHostname === "localhost" || currentHostname === "192.168.2.140") {
-				// targetHostname = "192.168.2.200:6581";
-				// targetprotocol = "https://";
-				//targetHostname = "192.168.2.140";
-				//targetHostname = "jmcam.goip.de";
+			// targetHostname = "192.168.2.200:6581";
+			// targetprotocol = "https://";
+			//targetHostname = "192.168.2.140";
+			//targetHostname = "jmcam.goip.de";
 			// } else {
 			// 	//targetHostname = "jmangels.goip.de";
 			// 	//targetHostname = "jmangels.goip.de:6580";
-				targetHostname = "jmangels.goip.de:6581";
-				targetprotocol = "https://";
+			targetHostname = "jmangels.goip.de:6581";
+			targetprotocol = "https://";
 			// }
 			// Create Global Model
 			var appVersion = this.getManifestEntry("/sap.app/applicationVersion/version");
@@ -50,7 +50,7 @@ sap.ui.define([
 				_URL: targetprotocol + targetHostname
 			});
 			this.setModel(oModel, "GLOBAL");
-			
+
 			//Load Flugbuchdata
 			this.loadData();
 
@@ -59,25 +59,45 @@ sap.ui.define([
 			var url;
 			var oModel = new JSONModel();
 			var oEntry = {};
-			
+
 			var oModelG = this.getModel("GLOBAL");
 			var _URL = oModelG.getProperty("/_URL");
 			
-			
+			// sap.ui.core.BusyIndicator.show();
+
 			url = _URL + "/FLUGBUCH/load_flugdaten.php";
 
-            var datum_start = new Date(2000, 0, 1, 0, 0, 0, 0); 
-			var datum_ende  = new Date(2100, 11, 31, 23, 59, 59, 0); 
-            var s_datum_start = datum_start.toISOString().substr(0, 19).replace('T', ' ');
-            var s_datum_ende  = datum_ende.toISOString().substr(0, 19).replace('T', ' ');
-            
+			var datum_start = new Date(2000, 0, 1, 0, 0, 0, 0);
+			var datum_ende = new Date(2100, 11, 31, 23, 59, 59, 0);
+			var s_datum_start = datum_start.toISOString().substr(0, 19).replace('T', ' ');
+			var s_datum_ende = datum_ende.toISOString().substr(0, 19).replace('T', ' ');
+
 			oEntry.TABLE = "FLUGBUCH";
 			oEntry.START = s_datum_start;
 			oEntry.ENDE = s_datum_ende;
-			
-			oModel.loadData(url, oEntry, true, "POST");
+
+			oModel.loadData(url, oEntry, false, "POST");
 			this.setModel(oModel, "DATA");
+			//oModel.refresh(true);
 			
+			// oModel.attachRequestCompleted(function () {
+			// 	sap.ui.core.BusyIndicator.hide();
+			// });
+		},
+		handleRefresh: function (evt,oView) {
+			// var reload = this.getModel("i18n").getResourceBundle().getText("reload");
+			// var oModel = this.getModel("DATA");
+			// // oModel.setData({modelData:{}});
+			// // oModel.updateBindings(true);
+			// if (oModel) {
+			// 	// oModel.setData(null);
+			// 	// oModel.updateBindings(true);
+			// 	oModel.setData([]);
+			// }
+			// var oTable = oView.byId("mainTable");
+			// oTable.getBinding("rows").refresh();
+			// this.loadData();
+			// sap.m.MessageToast.show(reload);
 		}
 	});
 });
--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -23,6 +23,41 @@ sap.ui.define([
 			//oTable.getBindingContext
 
 		},
+		handleFullscreen1: function (evt) {
+			var path = jQuery.sap.getModulePath("de.mangels.zfb.ZFLUGBUCH");
+			switch (path) {
+			case ".":
+				var oCore = sap.ui.getCore();
+				if (!this._oShell) {
+					//	this._oShell = sap.ui.getCore().byId("Shell1");
+					this._oShell = oCore.byId("Shell1");
+				}
+
+				if (evt.getSource().getPressed()) {
+					//MessageToast.show(evt.getSource().getId() + " Pressed");
+					this._oShell.setAppWidthLimited(false);
+				} else {
+					//MessageToast.show(evt.getSource().getId() + " Unpressed");
+					this._oShell.setAppWidthLimited(true);
+					//this._getSplitApp().setMode('ShowHideMode');
+				}
+
+				break;
+			default:
+				var config = sap.ushell.services.AppConfiguration;
+
+				if (evt.getSource().getPressed()) {
+					config.setApplicationFullWidth(true);
+				} else {
+					config.setApplicationFullWidth(false);
+				}
+
+				break;
+			}
+		},
+		onRefresh: function (oEvent) {
+			this.getOwnerComponent().handleRefresh(oEvent,this.getView());
+		},
 		sortLfdnr: function (oEvent) {
 			var oView = this.getView();
 			var oTable = oView.byId("mainTable");
